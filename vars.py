3471ad00e28ff8a83a12185b345062e9d18a6947
goyalgaurav1213-collab <goyalgaurav1213@gmail.com>
2026-02-05 10:23:37 +0530

    Update owner and credit information in vars.py

--- vars.py
+++ vars.py
@@ -7,16 +7,17 @@ API_ID = int(environ.get("API_ID", "xxxxxxxxx"))
 API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxxxxx")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "7456681709"))
-CREDIT = environ.get("CREDIT", "R̸a̸h̸u̸l̸")
+OWNER = int(environ.get("OWNER", "738067141"))
+CREDIT = environ.get("CREDIT", "GG BOTS")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '7456681709').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '738067141').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '7456681709').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '738067141').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
